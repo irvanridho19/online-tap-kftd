f85109f88304fbbc110ba6a1617455d9f3482774
R M Irfan Ridho <irvanridho@live.com>
2026-01-06 16:48:56 +0700

    fix: add turbopack config for Next 16 build on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
 };
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
